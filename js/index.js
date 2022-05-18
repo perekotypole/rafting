3e5f344a0cb1f23a9fc7aea627fcca48a3711060
perekotypole <parcersara@gmail.com>
2022-05-18 22:22:34 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,2 @@
+const langButton=document.getElementById("lang"),langs=["ua","en"];function changeLang(){let n=langs.indexOf(langButton.innerText.toLowerCase())+1;n=n>=langs.length?0:n,langButton.innerText=langs[n]}
+//# sourceMappingURL=index.js.map
